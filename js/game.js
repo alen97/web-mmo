923aabe2e700ee4a81e33f14e1a811360d773534
Gabriel Alén Gauna <gabrielg@epidataconsulting.com>
2020-05-20 22:16:54 -0300

    update

--- js/game.js
+++ js/game.js
@@ -29,12 +29,19 @@ Game.create = function(){
     // proyectiles.setAll('checkWorldBounds', true);
     // proyectiles.setAll('outOfBoundsKill', true);
     
+    
     game.canvas.oncontextmenu = function (e) { e.preventDefault(); }
 
     Client.askNewPlayer();
 };
 
+// var shoot = false;
+// var timer;
+// var total = 0;
+
 Game.update = function(){
+
+
     //  only move when you click
     if (game.input.activePointer.isDown)
     {
@@ -43,17 +50,38 @@ Game.update = function(){
         // proyectilSprite.physics.enable(proyectilSprite, Phaser.Physics.ARCADE);
         // proyectilSprite.body.allowRotation = false;
         // game.physics.arcade.moveToPointer(bullet, 300);
+    
+        // shoot = true;
 
         Game.getCoordinates(game.input.activePointer);
+
+        // //  Create our Timer
+        // this.timer = game.time.create(false);
+
+        // //  Set a TimerEvent to occur after 2 seconds
+        // this.timer.loop(1000, updateCounter, this);
+
+        // //  Start the timer running - this is important!
+        // //  It won't start automatically, allowing you to hook it to button events and the like.
+        // this.timer.start();
     }
     //proyectilSprite.rotation = game.physics.arcade.angleToPointer(proyectilSprite);
 
+
+    // if(total > 0 && shoot == true) {
+    //     this.timer.stop();
+    //     this.shoot = false;
+    // }
 
     // if (game.input.activePointer.isDown)
     // {
     // }
 
 }
+
+// function updateCounter() {
+//     total++;
+// }
 
 Game.getCoordinates = function(pointer){
     console.log(pointer);
